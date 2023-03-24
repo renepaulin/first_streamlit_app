b2c35091f79ecb4fab5c576eec5376bbc180c431
renepaulin <128854833+renepaulin@users.noreply.github.com>
2023-03-24 15:17:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title('My parents New Healthy diner')
+streamlit.title('My Mom''s New Healthy diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('Hard boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+   
